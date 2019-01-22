0eb5c82d98969f0ff6d7a0391ec5e2f59d82baab
Luca Guidi <me@lucaguidi.com>
2019-01-22 11:14:09 +0100

    Bump version

--- lib/hanami/reloader/version.rb
+++ lib/hanami/reloader/version.rb
@@ -2,6 +2,6 @@
 
 module Hanami
   module Reloader
-    VERSION = "0.2.1"
+    VERSION = "1.0.0.alpha1"
   end
 end
--- spec/unit/hanami/reloader/version_spec.rb
+++ spec/unit/hanami/reloader/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Reloader::VERSION" do
   it "exposes version" do
-    expect(Hanami::Reloader::VERSION).to eq("0.2.1")
+    expect(Hanami::Reloader::VERSION).to eq("1.0.0.alpha1")
   end
 end
