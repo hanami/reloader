5826b52778efefe075c914242df26c9a43aafb6f
Tim Riley <tim@riley.id.au>
2024-07-15 21:28:06 +1000

    Prepare for v2.2.0.beta1

--- spec/unit/hanami/reloader/version_spec.rb
+++ spec/unit/hanami/reloader/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Reloader::VERSION" do
   it "exposes version" do
-    expect(Hanami::Reloader::VERSION).to eq("2.1.0")
+    expect(Hanami::Reloader::VERSION).to eq("2.2.0.beta1")
   end
 end
